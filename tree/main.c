eafa3db2c71710b88c3b5ac75af189bac72e781d
Данила Печенин <dpechenin@icloud.com>
2024-04-28 15:58:01 +0300

    Initial Commit

---
+++ tree/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  tree
+//
+//  Created by Данила Печенин on 28.04.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
